1397aa0546317194369d0f71f36f7d2274be4ffb
sophimkep <89367927+sophimkep@users.noreply.github.com>
2022-02-20 12:08:59 +0700

    Add files via upload

---
+++ authentication.php
@@ -0,0 +1,22 @@
+<?php 
+    include('connection.php');
+    $username = $_POST['user'];
+    $password = $_POST['pass'];
+
+    $username = stripcslashes($username);
+    $password = stripcslashes($password);
+    $username = mysqli_real_escape_string($con,$username);
+    $password = mysqli_real_escape_string($con,$password);
+
+    $sql = "select * from login where username='$username' and password = '$password'";
+    $result = mysqli_query($con,$sql);
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $count = mysqli_num_rows($result);
+
+    if($count==1){
+        echo 'Welcome '.$username;
+    }else {
+        echo "<h1><center>Login failed. Invalid username or password.</center></h1>";
+        require_once('index.html');
+    }
+?>
